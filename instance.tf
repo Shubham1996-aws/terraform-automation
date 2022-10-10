8c7d67eee80416b9be51d2ff2c0d05587ae78856
Shubham1996-aws <69294193+Shubham1996-aws@users.noreply.github.com>
2022-10-10 21:32:20 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web-server" {
     security_groups = [aws_security_group.web-sg.name]
     user_data = "${file("userdata.sh")}"
     tags = {
-      "Name" = "server-${count.index}"
+      "Name" = "Machine-${count.index}"
     }
   
 } 
