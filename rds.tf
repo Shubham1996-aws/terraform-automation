c495c082f7a68a84e3c286a051590fa88dfe4e5e
Shubham1996-aws <69294193+Shubham1996-aws@users.noreply.github.com>
2022-10-10 21:33:59 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,7 +1,7 @@
  resource "aws_db_instance" "myrds" {
    allocated_storage   = 20
    storage_type        = "gp2"
-   identifier          = "rdstf1"
+   identifier          = "albrdstf"
    engine              = "mysql"
    engine_version      = "8.0.27"
    instance_class      = "db.t2.micro"
@@ -11,6 +11,6 @@
    skip_final_snapshot = true
 
    tags = {
-     Name = "myrds"
+     Name = "albmyrds"
    }
  }
